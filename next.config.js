d21eab9d4b6f3d283db994b8ecfc7bccfc85c9d5
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-10-14 00:40:06 +0400

    feat(products-list): add

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            { hostname: 'hips.hearstapps.com' }, { hostname: 'static.pcbuilder.net' }
+        ]
+    },
     experimental: {
         serverActions: true,
     },
