66e3cf10bbf09d9e0baedbad11a9ef58e5e67e23
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-10-14 18:36:57 +0400

    feat(search-results): add

---
+++ src/app/search/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+
+import ProductCard from "@/components/reusables/ProductCard";
+
+import { prisma } from "@/lib/db/prisma";
+
+interface SearchPageProps {
+  searchParams: { query: string };
+}
+
+export function generateMetadata({
+  searchParams: { query },
+}: SearchPageProps): Metadata {
+  return {
+    title: `Search: ${query} - Flowmazon`,
+  };
+}
+
+export default async function SearchPage({
+  searchParams: { query },
+}: SearchPageProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    orderBy: { id: "desc" },
+  });
+
+  if (products.length === 0) {
+    return <div className="text-center">No products found</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="mb-6 text-4xl font-bold">Search results for: {query}</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
